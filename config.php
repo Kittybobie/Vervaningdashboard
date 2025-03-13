a28a30201eeba8168abf73c29383f879d47cb856
Kittybobie <alessandro.deschuymer@gmail.com>
2025-03-13 10:28:09 +0100

    config

--- config.php
+++ config.php
@@ -9,5 +9,7 @@ $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Database verbinding mislukt: " . $conn->connect_error);
+}else {
+    echo "Database connection successful!";
 }
 ?>
