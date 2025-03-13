895a3a3d157b85601fe101ba5df1e9f97f5f3440
Kittybobie <alessandro.deschuymer@gmail.com>
2025-03-13 10:37:27 +0100

    config2

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
-$host = "bkqrr4fpabrirltwrq38-mysql.services.clever-cloud.com"; // MYSQL_ADDON_HOST
-$user = "u9pgwjuq27e3npxv"; // MYSQL_ADDON_USER
-$pass = "xqwOSonP0LnDFUMcqZDN"; // MYSQL_ADDON_PASSWORD
-$dbname = "aanwezigheidsdashboard"; // <-- Hier zet je jouw database naam
-$port = 3306; // MYSQL_ADDON_PORT
+$host = "bkqrr4fpabrirltwrq38-mysql.services.clever-cloud.com"; 
+$user = "u9pgwjuq27e3npxv";
+$password = "xqwOSonP0LnDFUMcqZDN";
+$database = "bkqrr4fpabrirltwrq38";
+$port = 3306;
 
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
-    die("Database verbinding mislukt: " . $conn->connect_error);
-}else {
-    echo "Database connection successful!";
+    die("Verbinding mislukt: " . $conn->connect_error);
 }
+
+// Zet de juiste tekenset voor de verbinding
+$conn->set_charset("utf8mb4");
 ?>
