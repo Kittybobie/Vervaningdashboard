d6476af9e6654b04a971eb3664324c047309cf0c
Kittybobie <alessandro.deschuymer@gmail.com>
2025-03-13 12:32:54 +0100

    change

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ $previous_day = $days_of_week[($current_day_index - 1 + count($days_of_week)) % 
 $next_day = $days_of_week[($current_day_index + 1) % count($days_of_week)];
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 // Haal de gegevens uit POST (of zet standaardwaarden)
 $hour = isset($_POST['hour']) ? (int)$_POST['hour'] : 1; // Standaard naar lesuur 1
 $status = $_POST['status'] ?? 'Onbekend';
@@ -55,12 +56,17 @@ $sql = "SELECT t.id, t.name AS teacher_name, a.hour, a.status, a.reason, a.tasks
         JOIN teachers t ON a.teacher_id = t.id
         WHERE LOWER(a.day) = LOWER(?)";
 =======
+=======
+>>>>>>> parent of 4fd3f5c (meerdere dagen)
 // SQL-query om de vervangingen op te halen
 $sql = "SELECT t.name AS teacher_name, a.day, a.hour, a.reason, a.tasks, a.status 
         FROM attendance a
         JOIN teachers t ON a.teacher_id = t.id
         WHERE a.status IN ('absent', 'meeting') AND a.day = ? 
         ORDER BY a.day, a.hour";
+<<<<<<< HEAD
+>>>>>>> parent of 4fd3f5c (meerdere dagen)
+=======
 >>>>>>> parent of 4fd3f5c (meerdere dagen)
 
 $stmt = $conn->prepare($sql);
