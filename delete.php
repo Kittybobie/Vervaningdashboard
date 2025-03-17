dc238c2038bff06941d67314835077aecbe33d8d
Kittybobie <alessandro.deschuymer@gmail.com>
2025-03-17 11:34:40 +0100

    button

--- delete.php
+++ delete.php
@@ -165,7 +165,9 @@ $result = $conn->query($sql);
             </tr>
         <?php endif; ?>
     </table>
-    <button type="submit" class="btn-save">Opslaan</button>
+    <form method="POST" style="display:inline;">
+        <button type="submit" class="btn-save">Opslaan</button>
+    </form>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
